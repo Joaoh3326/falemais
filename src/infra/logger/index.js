226b5ee4e35a7f3683d48507645cfe7753c02e30
joaoh3326 <joaoh3326@gmail.com>
2021-08-18 22:36:13 -0300

    ajuste logger

--- src/infra/logger/index.js
+++ src/infra/logger/index.js
@@ -1,10 +1,16 @@
-const pino = require('pino')({
+let configPino = {
   level: 'debug',
   prettyPrint: {
     levelFirst: true,
     colorize: true,
   },
-});
+};
+
+if (process.env.NODE_ENV === 'production') {
+  configPino = { level: 'debug' };
+}
+
+const pino = require('pino')(configPino);
 
 const pinoHttp = require('pino-http')({ logger: pino });
 
